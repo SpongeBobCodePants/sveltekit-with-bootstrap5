df370c72a93a801ce278ddb6b642539e54d8a76d
SpongeBobCodePants <matt.mercer73@gmail.com>
2024-01-21 14:06:33 -0500

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>import "../app.scss";
+import scriptSrc from 'bootstrap/dist/js/bootstrap.bundle.js?url';
+</script>
+<svelte:head>
+  <script src={scriptSrc}></script>
+</svelte:head>
+<slot></slot>
